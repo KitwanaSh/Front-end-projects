569e0fe98440de8722b504d9f7126e04e2f9b092
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-01-18 19:56:04 +0200

    Fix: Update the css

--- passwaord_generator/index.js
+++ passwaord_generator/index.js
@@ -1,2 +1,11 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let passwordEl1 = document.getElementById("password-el_!")
+let passwordEl2 = document.getElementById("password-el_1")
+
+function createpass() {
+	let makeIndex = Math.floor(Math.random() * characters.length)
+	passwordEl1.textContent += characters[makeIndex]
+	
+}
+
