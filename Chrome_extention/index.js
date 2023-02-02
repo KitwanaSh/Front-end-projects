3bdb19dbf821cb3f51dc3290baceb0cdaf66588e
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-02-02 07:00:13 +0200

    Feat: add the chrome extention

---
+++ Chrome_extention/index.js
@@ -0,0 +1,56 @@
+let myLeads = []
+let oldLeads = []
+const inputEl = document.getElementById("input-el")
+const saveEl = document.getElementById("input-btn")
+const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage
+
+	render()
+}
+const tabs = [
+	{url: "https://www.linkedin.com/in/kitwanasheb"}
+]
+tabBtn.addEventListener("click", function() {
+	console.log(tabs[0].url)
+	myLeads.push(tabs[0].url)
+	localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+	render(myLeads)
+})
+
+function render(leads) {
+	let listItems = ""
+	for (let i = 0; i < leads.length; i++) {
+		listItems += `
+			<li>
+				<a target = "_blank" href = "${leads[i]}">${leads[i]}</a>
+			</li>
+		`
+	}
+	ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+	localStorage.clear()
+	myLeads = []
+	render(myLeads)
+})
+
+
+saveEl.addEventListener("click", function() {
+	myLeads.push(inputEl.value)
+	inputEl.value = ""
+	// Save the myLeads array to an local storage
+	localStorage.setItem("myLeads", JSON.stringify(myLeads))
+	render(myLeads)
+})
+
+
