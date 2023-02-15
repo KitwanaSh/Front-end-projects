1470b4b154611dff445f313f43c5cf1ffb136aba
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-02-15 20:28:52 +0200

    Feat: add another space

--- passwaord_generator/index.js
+++ passwaord_generator/index.js
@@ -3,21 +3,31 @@ const char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q
 
 let passwordEl = document.getElementById("password-el")
 let createPassEl = document.getElementById("createpass")
-// let passwordEl2 = document.getElementById("password-el2")
-
-// createPassEl.addEventListener("click", function() {
-// 	let ran1 = Math.floor( Math.random() * char.length)
-// 	let ran2 = Math.floor( Math.random() * char.length)
-// 	let ran3 = Math.floor( Math.random() * char.length)
-// 	let ran4 = Math.floor( Math.random() * char.length)
-// 	let ran5 = Math.floor( Math.random() * char.length)
-// 	let ran6 = Math.floor( Math.random() * char.length)
-// 	let ran7 = Math.floor( Math.random() * char.length)
-// 	let ran8 = Math.floor( Math.random() * char.length)
-
-// 	passwordEl.textContent = char[ran1] + char[ran2] + char[ran3] + char[ran4] + char[ran5] + char[ran6] + char[ran7] + char[ran8]
-// })
+let passwordEl2 = document.getElementById("password-el2")
 
 createPassEl.addEventListener("click", function() {
-	
+	let ran1 = Math.floor( Math.random() * char.length)
+	let ran2 = Math.floor( Math.random() * char.length)
+	let ran3 = Math.floor( Math.random() * char.length)
+	let ran4 = Math.floor( Math.random() * char.length)
+	let ran5 = Math.floor( Math.random() * char.length)
+	let ran6 = Math.floor( Math.random() * char.length)
+	let ran7 = Math.floor( Math.random() * char.length)
+	let ran8 = Math.floor( Math.random() * char.length)
+
+	passwordEl.textContent = char[ran1] + char[ran2] + char[ran3] + char[ran4] + char[ran5] + char[ran6] + char[ran7] + char[ran8]
+})
+
+
+createPassEl.addEventListener("click", function() {
+	let ran1 = Math.floor( Math.random() * char.length)
+	let ran2 = Math.floor( Math.random() * char.length)
+	let ran3 = Math.floor( Math.random() * char.length)
+	let ran4 = Math.floor( Math.random() * char.length)
+	let ran5 = Math.floor( Math.random() * char.length)
+	let ran6 = Math.floor( Math.random() * char.length)
+	let ran7 = Math.floor( Math.random() * char.length)
+	let ran8 = Math.floor( Math.random() * char.length)
+
+	passwordEl2.textContent = char[ran1] + char[ran2] + char[ran3] + char[ran4] + char[ran5] + char[ran6] + char[ran7] + char[ran8]
 })
