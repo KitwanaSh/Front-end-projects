fa08c1e72de9298298e7bc3b4e7fc607ddb536bd
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-02-15 20:19:43 +0200

    Fix: Fist debub

--- passwaord_generator/index.js
+++ passwaord_generator/index.js
@@ -1,14 +1,23 @@
-let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-let passwordEl1 = document.getElementById("password-el_!")
-let passwordEl2 = document.getElementById("password-el_1")
+const char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
 
-function createpass() {
-	let makeIndex = Math.floor(Math.random() * characters.length)
-	for (let i = 0; i < makeIndex.length; i++) {
-		console.log(makeIndex[i])
-	}
-	//passwordEl1.textContent += characters[makeIndex]
+let passwordEl = document.getElementById("password-el")
+let createPassEl = document.getElementById("createpass")
+// let passwordEl2 = document.getElementById("password-el2")
+
+// createPassEl.addEventListener("click", function() {
+// 	let ran1 = Math.floor( Math.random() * char.length)
+// 	let ran2 = Math.floor( Math.random() * char.length)
+// 	let ran3 = Math.floor( Math.random() * char.length)
+// 	let ran4 = Math.floor( Math.random() * char.length)
+// 	let ran5 = Math.floor( Math.random() * char.length)
+// 	let ran6 = Math.floor( Math.random() * char.length)
+// 	let ran7 = Math.floor( Math.random() * char.length)
+// 	let ran8 = Math.floor( Math.random() * char.length)
+
+// 	passwordEl.textContent = char[ran1] + char[ran2] + char[ran3] + char[ran4] + char[ran5] + char[ran6] + char[ran7] + char[ran8]
+// })
+
+createPassEl.addEventListener("click", function() {
 	
-}
-
+})
