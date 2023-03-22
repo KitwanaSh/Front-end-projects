950557e0e4a70d5e9a1a8446a2029601b852f575
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-03-22 19:25:39 +0200

    Refractor: Make the code DRY

--- Color_generator_API/index.js
+++ Color_generator_API/index.js
@@ -5,10 +5,8 @@ const submitColor = document.getElementById("submit-color")
 fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=7")
     .then( resp => resp.json())
     .then( data => {
-        let colorsArray = data.colors
         let html = ""
-        for(let colorA of colorsArray){
-            // console.log(colorA.name)
+        for(let colorA of data.colors){
             html += `
             <option value = "${colorA.name.closest_named_hex}">${colorA.name.value}</option>
             `
@@ -17,21 +15,10 @@ fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=7")
         
     })
 
-// let sol = document.getElementById("sol")
-
-// function getText() {
-//     Object.values(selectColor.options).
-//     forEach(function (option) {
-//         console.log(option.value)
-//         sol.innerHTML = option.value
-//     })
-// }
-
 function handleSubmit(event) {
     event.preventDefault();
     let selectedOption = document.getAnimations("select-color")
     repr.style.backgroundColor = selectColor.value
-    // sol.innerText = selectColor.value
 }
 
 submitColor.addEventListener("click", handleSubmit)
@@ -41,7 +28,6 @@ fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=5")
     .then( data => {
         let getHtml = ""
         for (let i of data.colors){
-            console.log(i.name.closest_named_hex)
             getHtml += `
             <div class = "hex-c">${i.name.closest_named_hex}</div>
             `
