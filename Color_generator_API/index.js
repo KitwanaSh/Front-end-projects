236dde9c09a4638b59b64bfe37e01a947ab1033a
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-03-22 15:21:39 +0200

    Fix: get the true hash

--- Color_generator_API/index.js
+++ Color_generator_API/index.js
@@ -1,4 +1,6 @@
 const selectColor = document.getElementById("select-color")
+const repr = document.getElementById("repre")
+const submitColor = document.getElementById("submit-color") 
 
 fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=7")
     .then( resp => resp.json())
@@ -6,10 +8,29 @@ fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=7")
         let colorsArray = data.colors
         let html = ""
         for(let colorA of colorsArray){
-            console.log(colorA.name)
+            // console.log(colorA.name)
             html += `
-            <option value = "${colorA.name.value}">${colorA.name.value}</option>
+            <option value = "${colorA.name.closest_named_hex}">${colorA.name.value}</option>
             `
         }
         selectColor.innerHTML = html
+        
     })
+
+let sol = document.getElementById("sol")
+
+// function getText() {
+//     Object.values(selectColor.options).
+//     forEach(function (option) {
+//         console.log(option.value)
+//         sol.innerHTML = option.value
+//     })
+// }
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let selectedOption = document.getAnimations("select-color")
+    sol.innerText = selectColor.value
+}
+
+submitColor.addEventListener("click", handleSubmit)
