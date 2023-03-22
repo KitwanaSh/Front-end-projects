ed5c8a23e76259931456c0d8e37a8ceab1aac949
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-03-22 10:06:04 +0200

    Fix: Update the Option tag in Js

--- Color_generator_API/index.js
+++ Color_generator_API/index.js
@@ -1,1 +1,15 @@
-// Some js codes here
+const selectColor = document.getElementById("select-color")
+
+fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=7")
+    .then( resp => resp.json())
+    .then( data => {
+        let colorsArray = data.colors
+        let html = ""
+        for(let colorA of colorsArray){
+            console.log(colorA.name)
+            html += `
+            <option value = "${colorA.name.value}">${colorA.name.value}</option>
+            `
+        }
+        selectColor.innerHTML = html
+    })
