baf3e8cd37f9e10c936d29970bb2ef65f85fc84b
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-03-25 18:22:23 +0200

    Feat: add the first ApI rule

--- Whilelist_movies/index.js
+++ Whilelist_movies/index.js
@@ -1,1 +1,25 @@
-// JS code will go here
+const searchMovies = document.getElementById("search-movies")
+const searchButton = document.getElementById("search-btn")
+const movieList = document.getElementById("movie-list")
+let movieTitle
+
+
+const handleSearch = () => {
+	movieTitle = searchMovies.value
+	fetch(`http://www.omdbapi.com/?apikey=634aecba&s=${movieTitle}`)
+	.then(resp => resp.json())
+	.then(data => {
+		console.log(data)
+		let moVies = []
+		for(let mov of data.Search) {
+			moVies += `
+			<h2>${mov.Title}</h2>
+			`
+		}
+		movieList.innerHTML = moVies
+	})
+}
+
+searchButton.addEventListener("click", handleSearch)
+
+
