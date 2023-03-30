cfafbf5f22510fbd4ed09f09944657fdd413ffd4
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-03-30 15:10:36 +0200

    Fix: Update

--- passwaord_generator/index.js
+++ passwaord_generator/index.js
@@ -1,4 +1,6 @@
-const char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g",
+"h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+"$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 
 let passwordEl = document.getElementById("password-el")
@@ -17,7 +19,6 @@ createPassEl.addEventListener("click", function() {
 
 	passwordEl.textContent = char[ran1] + char[ran2] + char[ran3] + char[ran4] + char[ran5] + char[ran6] + char[ran7] + char[ran8]
 })
-
 
 createPassEl.addEventListener("click", function() {
 	let ran1 = Math.floor( Math.random() * char.length)
