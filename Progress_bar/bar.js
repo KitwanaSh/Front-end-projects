eceb5c9bd445d27ce9f8def2bb0ae2394fd38785
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-18 20:04:59 +0200

    Feat: Add the progress bar

---
+++ Progress_bar/bar.js
@@ -0,0 +1,15 @@
+const progress = document.querySelector(".progress")
+const loading = document.querySelector(".loading")
+
+let i = 0
+const fakeUpload = [0, 10, 20, 40, 50, 60, 70, 80, 90, 100]
+
+const interval = setInterval(() => {
+    progress.style.width = `${fakeUpload[i]}%`
+    loading.innerHTML = fakeUpload[i] + "%"
+    i++
+    if(i == fakeUpload.length) {
+        clearInterval(interval)
+        loading.innerHTML = "Completed"
+    }
+}, 1000)
